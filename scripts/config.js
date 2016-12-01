dd938539283de250f5dc1fc58b818cd58a095d83
James Honzatko <bejbej.uit@gmail.com>
2016-12-01 17:53:59 -0600

    v0.5

--- scripts/config.js
+++ scripts/config.js
@@ -8,6 +8,7 @@ var appConfig = {
     statCategories: [
         { name: "cmc", types: ["creature", "artifact", "enchantment", "planeswalker", "instant", "sorcery"] }
     ],
+    cardCacheLimit: 1000,
     cardsUrl: "https://api.deckbrew.com/mtg/cards",
     decksUrl: "https://uit.pythonanywhere.com/deck/default/stashes",
     imagesUrl: "https://image.deckbrew.com/mtg/multiverseid/",
