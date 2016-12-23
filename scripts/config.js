c3db6875e7d81b353a3637809bf6354b89fb6b63
James Honzatko <bejbej.uit@gmail.com>
2016-12-22 18:03:56 -0600

    v0.15

--- scripts/config.js
+++ scripts/config.js
@@ -4,7 +4,7 @@ var appConfig = {
         { name: "cmc", types: ["creature", "artifact", "enchantment", "planeswalker", "instant", "sorcery"] }
     ],
     cardCacheLimit: 1000,
-    cardsUrl: "https://api.deckbrew.com/mtg/cards",
+    cardsUrl: "https://mtgdecks-api.herokuapp.com/api/cards",
     decksUrl: "https://mtgdecks-api.herokuapp.com/api/decks",
     imagesUrl: "https://image.deckbrew.com/mtg/multiverseid/",
     storeUrl: "http://shop.tcgplayer.com/productcatalog/product/show?ProductName="
