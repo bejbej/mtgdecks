132ff2d48683f99c3079004574b90e7277946f90
James Honzatko <bejbej.uit@gmail.com>
2017-05-13 17:00:58 -0500

    Grouping by type groups evengly.

--- src/app/components/cardSet/groupByType.ts
+++ src/app/components/cardSet/groupByType.ts
@@ -7,29 +7,27 @@ module app {
     class GroupByType implements ng.IDirective {
         
         private groupByType = (cards: Card[]): CardSet[][] => {
-            var columns = [
-                ["creature"],
-                ["artifact", "enchantment", "planeswalker", "land"],
-                ["instant", "sorcery"],
-            ];
+            var types = ["creature", "artifact", "enchantment", "planeswalker", "land", "instant", "sorcery"];
 
-            return columns.map(column => {
-                return column.reduce((array, type) => {
-                    var cardSet = new CardSet();
-                    cardSet.name = type;
-                    cardSet.cards = cards.filter(card => {
-                        return card.primaryType === type;
-                    });
-                    cardSet.numberOfCards = cardSet.cards.reduce((count, card) => {
-                        return count + card.quantity;
-                    }, 0);
+            var cardSets = types.reduce((array, type) => {
+                var cardSet = new CardSet();
+                cardSet.name = type;
+                cardSet.cards = cards.filter(card => {
+                    return card.primaryType === type;
+                });
+                cardSet.numberOfCards = cardSet.cards.reduce((count, card) => {
+                    return count + card.quantity;
+                }, 0);
 
-                    if (cardSet.cards.length > 0) {
-                        array.push(cardSet);
-                    }
+                if (cardSet.cards.length > 0) {
+                    array.push(cardSet);
+                }
 
-                    return array;
-                }, []);
+                return array;
+            }, []);
+
+            return new GroupEvenly().exec(cardSets, 3, cardSet => {
+                return cardSet.cards.length + 4;
             });
         }
 
