28b7c06ec2a730ca8e1908a900e240434ff77105
James Honzatko <bejbej.uit@gmail.com>
2017-07-02 01:13:35 -0500

    Changed the image source.

--- src/app/entities/card.ts
+++ src/app/entities/card.ts
@@ -7,10 +7,24 @@ module app {
         cmc: number;
         multiverseId: number;
 
+        private _imageUrl: string;
+        private static _pattern = /{([^}]*)}/;
+
         constructor(private config: IConfig) { }
 
         imageUrl = (): string => {
-            return this.config.imagesUrl + this.multiverseId + ".jpg";
+            if (this._imageUrl != undefined) {
+                return this._imageUrl;
+            }
+
+            var match = Card._pattern.exec(this.config.imagesUrl);
+            if (match === null) {
+                this._imageUrl = "";
+                return this._imageUrl;
+            }
+
+            this._imageUrl = this.config.imagesUrl.replace(Card._pattern, this[match[1]]);
+            return this._imageUrl;
         }
 
         storeUrl = (): string => {
